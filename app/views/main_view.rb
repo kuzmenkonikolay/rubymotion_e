b8699c0ec46d397e66291d686a95b799b25f1e47
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2018-04-29 01:14:35 +0300

    autolayouts

--- app/views/main_view.rb
+++ app/views/main_view.rb
@@ -9,26 +9,55 @@ class MainView < UIView
     @button.setTitle('Pressing me', forState: UIControlStateHighlighted)
     @button.setTitleColor(UIColor.redColor, forState: UIControlStateNormal)
     # button.frame = [[x,y], [width, height]]
-    @button.frame = [[20,200], [250, 30]]
     addSubview(@button)
 
     @todo_button = UIButton.buttonWithType(UIButtonTypeDetailDisclosure)
     @todo_button.setTitle('Add Todo', forState: UIControlStateNormal)
     @todo_button.setTitleColor(UIColor.blueColor, forState: UIControlStateNormal)
     # button.frame = [[x,y], [width, height]]
-    @todo_button.frame = [[20,300], [300, 30]]
     addSubview(@todo_button)
 
     label = UILabel.new
     label.text = 'Text Label'
     label.textColor = UIColor.blueColor
-    label.frame = [[20,150], [250, 30]]
     addSubview(label)
 
     field = UITextField.new
     field.placeholder = 'Text inside the field'
     field.backgroundColor = UIColor.yellowColor
-    field.frame = [[20,250], [250, 30]]
     addSubview(field)
+
+    label.translatesAutoresizingMaskIntoConstraints = false
+    field.translatesAutoresizingMaskIntoConstraints = false
+    @button.translatesAutoresizingMaskIntoConstraints = false
+    @todo_button.translatesAutoresizingMaskIntoConstraints = false
+
+    constraints = NSLayoutConstraint.constraintsWithVisualFormat(
+        '|-50-[label][field(==label)]-50-|',
+        options: NSLayoutFormatAlignAllBaseline,
+        metrics: nil,
+        views: {'label' => label, 'field' => field})
+    constraints2 = NSLayoutConstraint.constraintsWithVisualFormat(
+        '|-50-[button]-50-|',
+        options: NSLayoutFormatDirectionLeadingToTrailing,
+        metrics: nil,
+        views: {'button' => @button})
+    constraints3 = NSLayoutConstraint.constraintsWithVisualFormat(
+        '|-50-[todo_button]-50-|',
+        options: NSLayoutFormatDirectionLeadingToTrailing,
+        metrics: nil,
+        views: {'todo_button' => @todo_button})
+    constraints4 = NSLayoutConstraint.constraintsWithVisualFormat(
+        'V:|-100-[label]-20-[button]-20-[todo_button]',
+        options: NSLayoutFormatDirectionLeadingToTrailing,
+        metrics: nil,
+        views: {'label' => label, 'button' => @button, 'todo_button' => @todo_button})
+
+    NSLayoutConstraint.activateConstraints(constraints)
+    NSLayoutConstraint.activateConstraints(constraints2)
+    NSLayoutConstraint.activateConstraints(constraints3)
+    NSLayoutConstraint.activateConstraints(constraints4)
+
+    self
   end
 end
