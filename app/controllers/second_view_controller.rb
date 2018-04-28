71076706afce0f6e6a721772665a8f73e54be46d
Nikolay Kuzmenko <kuzmenkojob@gmail.com>
2018-04-28 16:57:28 +0300

    remove and add view

--- app/controllers/second_view_controller.rb
+++ app/controllers/second_view_controller.rb
@@ -11,8 +11,14 @@ class SecondViewController < UIViewController
   def on_action
     if view.switch.on?
       puts 'on'
+      @text_label = UILabel
+      @text_label.text = 'This is hidden no more!'
+      @text_label.frame = [[20,300], [300, 30]]
+      @text_label.textColor = UIColor.blackColor
+      view.addSubview(@text_label)
     else
       puts 'of'
+      @text_label.removeFromSuperview
     end
   end
 end
